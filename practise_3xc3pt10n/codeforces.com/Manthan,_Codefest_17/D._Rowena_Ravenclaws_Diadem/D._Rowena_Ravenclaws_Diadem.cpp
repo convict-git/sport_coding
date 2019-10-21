0695da5e16a43b327bad622a8c8f9eb8347c7411
convict-git <official.mr.convict@gmail.com>
2019-10-22 00:35:56 +0530

    Update

--- practise_3xc3pt10n/codeforces.com/Manthan,_Codefest_17/D._Rowena_Ravenclaws_Diadem/D._Rowena_Ravenclaws_Diadem.cpp
+++ practise_3xc3pt10n/codeforces.com/Manthan,_Codefest_17/D._Rowena_Ravenclaws_Diadem/D._Rowena_Ravenclaws_Diadem.cpp
@@ -74,6 +74,102 @@ void err(istream_iterator<string> it, T a, Args... args) {
 
 signed main() {
    IOS; PREC;
+   int n;
+   cin >> n;
+   vvi T(n, vi());
+   vp pi(n, {-1, -1});
+
+   for (int i = 0; i < n; ++i) {
+      int u, t;
+      cin >> u >> t;
+      if (u != -1) --u;
+      pi[i].x = u;
+      pi[i].y = t;
+      if (pi[i].x != -1) {
+         T[pi[i].x].push_back(i);
+      }
+   }
+
+   const int D = 21;
+   vi cc(n, -1), depth(n, 0);
+   vvi table(n, vi(D+1, -1));
+   vvi Sdp(n, vi(D+1, false)), Pdp(n, vi(D+1, false));
+
+   function <void(int, int, int)> dfs = [&] (int u, int d, int cc_idx) -> void {
+      cc[u] = cc_idx;
+      depth[u] = d;
+
+      for (int k = 1; k <= D; ++k) if (table[u][k-1] != -1) {
+         table[u][k] = table[table[u][k-1]][k-1];
+         Sdp[u][k] = Sdp[u][k-1] & Sdp[table[u][k-1]][k-1];
+         Pdp[u][k] = Pdp[u][k-1] & Pdp[table[u][k-1]][k-1];
+      }
+
+      for (int v : T[u]) {
+         table[v][0] = u;
+         if (pi[v].y == 0) Sdp[v][0] = true;
+         else if (pi[v].y == 1) Pdp[v][0] = true;
+         dfs(v, d + 1, cc_idx);
+      }
+   };
+
+   for (int i = 0, cc_idx = 0; i < n; ++i) if (pi[i].x == -1) {
+      dfs(i, 0, cc_idx++);
+   }
+
+   auto walk = [&] (int u, int l, bool &pok, bool &sok) -> int {
+      sok = pok = true;
+      for (int d = 0; d <= D && u >= 0; ++d)
+         if ((1 << d) & l)  {
+            pok &= Pdp[u][d], sok &= Sdp[u][d];
+            u = table[u][d];
+         }
+      return u;
+   };
+
+   auto give_lca = [&] (int u, int v) -> int {
+      bool tmp1, tmp2;
+      if (depth[v] > depth[u]) v = walk(v, depth[v] - depth[u], tmp1, tmp2);
+      if (depth[u] > depth[v]) u = walk(u, depth[u] - depth[v], tmp1, tmp2);
+      if (u == v) return u;
+
+      for (int d = D; d >= 0; --d) {
+         if (table[u][d] == table[v][d]) continue;
+         u = table[u][d], v = table[v][d];
+      }
+      return table[u][0];
+   };
+
+   int q;
+   cin >> q;
+   while (q--) {
+      int u, v, t;
+      cin >> t >> u >> v;
+      --u, --v;
+      if (cc[u] != cc[v] || u == v) cout << "NO\n";
+      else {
+         int z = give_lca(u, v);
+         if (t == 1) {
+            if (u != z || depth[v] <= depth[u]) cout << "NO\n";
+            else {
+               bool tmp, ok;
+               walk(v, depth[v] - depth[z], tmp, ok);
+               if (u == z && ok) cout << "YES\n";
+               else cout << "NO\n";
+            }
+         }
+         else {
+            if (z == v && depth[v] <= depth[u]) cout << "NO\n";
+            else {
+              bool pok = true, sok = true, tmp1, tmp2;
+              walk(v, depth[v] - depth[z], pok, tmp2);
+              walk(u, depth[u] - depth[z], tmp1, sok);
+              if (pok && sok) cout << "YES\n";
+              else cout << "NO\n";
+            }
+         }
+      }
+   }
 
    return EXIT_SUCCESS;
 }
