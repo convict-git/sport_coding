69e00d73cad496815880e3a887d7135da6ae5a6b
convict-git <official.mr.convict@gmail.com>
2020-09-22 02:31:56 +0530

    Fixed Modular Arith Lib

--- cplib/snippets/Mod.cpp
+++ cplib/snippets/Mod.cpp
@@ -1,26 +1,29 @@
-#WARNING Is it 998244353?
 const int Mod = (int)1e9 + 7;
-inline int add (int x, int y) { return (1ll * x + y + (Mod << 1)) % Mod; }
-inline int sub (int x, int y) { return (1ll * x - y + (Mod << 1)) % Mod; }
-inline int mul (int x, int y) { return (int) (1ll*(x + Mod) * (y + Mod) % Mod); }
-inline int pow (int x, int y) {
-  int res = 1;
-  while (y) {
-    if (y & 1) res = mul(res, x);
-    x = mul(x, x), y >>= 1;
-  }
-  return res;
+// const int Mod = 998244353;
+#warning is it 998244353?
+#warning call_init_fac
+#define M(x) ((int)((x) % Mod))
+
+inline int add (int x, int y) { return M(1ll*M(x) + M(y) + (Mod << 1)); }
+inline int sub (int x, int y) { return M(1ll*M(x) - M(y) + (Mod << 1)); }
+inline int mul (int x, int y) { return (int) M(1ll*(M(x) + Mod) * (M(y) + Mod)); }
+inline int pw (int x, long long y) {
+   int res = 1;
+   while (y) {
+      if (y & 1ll) res = mul(res, x);
+      x = mul(x, x), y >>= 1ll;
+   }
+   return res;
 }
-inline int inv (int x) { return pow(x, Mod - 2); }
+inline int inv (int x) { return pw(x, 1ll*Mod - 2); }
 inline int dv (int x, int y) { return mul(x, inv(y)); }
 
-const int MAXFAC = (int)1e6 + 10;
+const int MAXFAC = (int)2e6 + 10;
 int fac[MAXFAC];
-void init_fac (int maxn) {
-#WARNING call_init_fac
-  fac[0] = 1;
-  for (int i = 1; i < maxn; ++i) fac[i] = mul(fac[i - 1], i);
+void init_fac (int maxn = MAXFAC) {
+   fac[0] = 1;
+   for (int i = 1; i < maxn; ++i) fac[i] = mul(fac[i - 1], i);
 }
-inline int C(int n, int r) {
-  return dv(fac[n], mul(fac[n-r], fac[r]));
+inline int nCr(int n, int r) {
+   return dv(fac[n], mul(fac[n-r], fac[r]));
 }
